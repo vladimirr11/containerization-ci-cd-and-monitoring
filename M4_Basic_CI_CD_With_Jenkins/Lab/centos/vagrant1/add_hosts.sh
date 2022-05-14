37e11be16862aee0cf501c90ee1c1bed2abecf51
Vladimir Stoev <vladimirstoevv@gmail.com>
2022-05-14 18:40:13 +0300

    Added lab files for basic ci/cd with Jenkins.

---
+++ M4_Basic_CI_CD_With_Jenkins/Lab/centos/vagrant1/add_hosts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "192.168.99.100 jenkins.do1.lab jenkins" >> /etc/hosts
---
+++ M4_Basic_CI_CD_With_Jenkins/Lab/debian/add_hosts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "192.168.99.100 jenkins.do1.lab jenkins" >> /etc/hosts
+echo "192.168.99.101 docker.do1.lab docker" >> /etc/hosts
